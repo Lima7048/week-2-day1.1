7329fdd93f75d993eb1ec290d4fb570318ffc051
salima <brycesalima@gmail.com>
2024-01-15 16:12:14 +0000

    while loops and looping over arrays (crashed using while loops

--- app1.js
+++ app1.js
@@ -4,6 +4,33 @@ for (let i = 0; i < 5; i++) {
   console.log("Iteration", i);
 }
 
+let videogames = [
+  "fox hole",
+  "eldenring",
+  "LOL",
+  "snake",
+  "candy crush",
+  "uno",
+];
 for (let z = 0; z < 7; z++) {
   console.log("Iteration", z);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(videogames[i]);
+}
+
+let arrayExample = [1, 2, 3];
+let z = 0;
+arrayExample[z];
+
+const foods = [
+  "pineapple",
+  "grape",
+  "matcha latte",
+  "avocado toast",
+  "salmon bagel",
+];
+for (let food of foods) {
+  console.log(food);
 }
