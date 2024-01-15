d8f3a71871b487c4457668baa410ff198ba75eb1
salima <brycesalima@gmail.com>
2024-01-15 13:53:23 +0000

    added an array list of my favorite food,colours and numbers

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Hello world!"); //Output: Hello world!
+
+const myblogPost = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+myblogPost[3] = "My fourth blog post";
+
+console.log(myblogPost[0]); // Output: My first blog post
+console.log(myblogPost[1]); // Output: My second blog post
+console.log(myblogPost[2]); // Output: My third blog post
+console.log(myblogPost[3]); // Output: My fourth blog post
+
+const myfavouriteFoods = ["Pasta", "Strawberry", "Brownies"]; // Output:'Pasta', 'Strawberry', 'Brownies'
+console.log(myfavouriteFoods);
+
+const myfavouriteColours = ["Green", "Pink", "Gold"]; // Output:'Green', 'Pink', 'Gold'
+console.log(myfavouriteColours);
+
+const myfavouriteNumbers = ["7", "24", "111"]; // Output:'7', '24', '111'
+console.log(myfavouriteNumbers);
+
+console.log(myfavouriteColours[1]);
+console.log(myfavouriteFoods[0]);
+console.log(myfavouriteNumbers[2]);
